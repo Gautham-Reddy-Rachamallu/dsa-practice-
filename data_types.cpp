62e02bdfa9cde8a95575dade0b68317d751fde3e
Gautham Reddy Rachamallu <rachamallugautham2006@gmail.com>
2026-07-04 09:38:44 +0530

    Day 1: datatypes and variables practice

---
+++ data_types.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age =23;
+    float amount=1.456f;
+    char letter='a';
+    bool male=true;
+    double log=1.234534;
+    cout<<sizeof(age)<<endl;//here we will get the size of the int ie 4 not the value of the variable 
+    cout<<letter<<endl;
+    cout<<amount<<endl;
+    cout<<male<<endl;
+    cout<<log<<endl;
+    return 0;
+
+} 
