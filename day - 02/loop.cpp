9b1f859f9bcf598c95f688f0443a73692bd15dec
Gautham Reddy Rachamallu <rachamallugautham2006@gmail.com>
2026-07-07 12:54:14 +0530

    Day:02 printing 1 to 5 numbers using for loop and while loop

--- day - 02/loop.cpp
+++ day - 02/loop.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 int main (){
     int n ;
+    //using for loop
     for(n=1;n<=5;n++){
         cout<<n<<endl;
+    }
+    //using while loop
+    int count =1,m=40;
+    while(count<=m){
+        cout<<count<<" ";
+        count++;
     }
 return 0;
 }
